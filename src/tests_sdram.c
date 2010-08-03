a0437752bb1ea990a57883e7b139ff2f020f18c3
lekernel <sebastien.bourdeauducq@lekernel.net>
2010-08-03 12:53:20 +0200

    Better SDRAM test

--- src/tests_sdram.c
+++ src/tests_sdram.c
@@ -19,7 +19,49 @@
 #include <stdio.h>
 #include "testdefs.h"
 
-#define TEST_WORDS (1024*1024)
+static void flush_caches()
+{
+	asm volatile(
+		"wcsr DCC, r0\n"
+		"nop\n"
+	);
+	flush_bridge_cache();
+}
+
+static int a8()
+{
+	unsigned int adr1, adr2;
+
+	adr1 = 0x40000000 + 0x1400000;
+	adr2 = 0x40000000 + 0x1000000;
+	*((unsigned int *)adr1) = 0x2991fe76;
+	*((unsigned int *)adr2) = 0x860eab97;
+	flush_caches();
+	if(*((unsigned int *)adr1) != 0x2991fe76) return TEST_STATUS_FAILED;
+	if(*((unsigned int *)adr2) != 0x860eab97) return TEST_STATUS_FAILED;
+	return TEST_STATUS_PASSED;
+}
+
+static int test_a(unsigned int a)
+{
+	unsigned int adr1, adr2;
+	
+	adr1 = 0x40000000 + 0x400000;
+	adr2 = adr1 | ((1024*4*4) << a);
+	*((unsigned int *)adr1) = (0x3a499876+a);
+	*((unsigned int *)adr2) = (0x08af8401+a);
+	flush_caches();
+	if(*((unsigned int *)adr1) != (0x3a499876+a)) return TEST_STATUS_FAILED;
+	if(*((unsigned int *)adr2) != (0x08af8401+a)) return TEST_STATUS_FAILED;
+	return TEST_STATUS_PASSED;
+}
+
+static int a9() { return test_a(9); }
+static int a10() { return test_a(10); }
+static int a11() { return test_a(11); }
+static int a12() { return test_a(12); }
+
+#define TEST_WORDS (512*1024)
 
 static int random()
 {
@@ -32,7 +74,7 @@ static int random()
 		test_buffer[i] = r;
 		r = 1664525*r + 1013904223;
 	}
-	flush_bridge_cache();
+	flush_caches();
 	r = 0;
 	for(i=0;i<TEST_WORDS;i++) {
 		if(test_buffer[i] != r)
@@ -49,7 +91,7 @@ static int hammer()
 
 	for(i=0;i<TEST_WORDS;i++)
 		test_buffer[i] = i & 1 ? 0xffffffff : 0x00000000;
-	flush_bridge_cache();
+	flush_caches();
 	for(i=0;i<TEST_WORDS;i++)
 		if(test_buffer[i] != (i & 1 ? 0xffffffff : 0x00000000))
 			return TEST_STATUS_FAILED;
@@ -63,7 +105,7 @@ static int crosstalk()
 
 	for(i=0;i<TEST_WORDS;i++)
 		test_buffer[i] = i & 1 ? 0xaaaaaaaa : 0x55555555;
-	flush_bridge_cache();
+	flush_caches();
 	for(i=0;i<TEST_WORDS;i++)
 		if(test_buffer[i] != (i & 1 ? 0xaaaaaaaa : 0x55555555))
 			return TEST_STATUS_FAILED;
@@ -71,6 +113,26 @@ static int crosstalk()
 }
 
 struct test_description tests_sdram[] = {
+	{
+		.name = "A8",
+		.run = a8
+	},
+	{
+		.name = "A9",
+		.run = a9
+	},
+	{
+		.name = "A10",
+		.run = a10
+	},
+	{
+		.name = "A11",
+		.run = a11
+	},
+	{
+		.name = "A12",
+		.run = a12
+	},
 	{
 		.name = "Random pattern",
 		.run = random
